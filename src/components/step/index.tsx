91a9ae592d24042bc851ae70af7efd9f0c42c786
Eduardo Fernando Farfus <eduardo.farfus@angellira.com.br>
2025-01-20 18:24:33 -0300

    Feita adição de biblioteca para ícones e adicionado ícones nos componentes com props

--- src/components/step/index.tsx
+++ src/components/step/index.tsx
@@ -1,13 +1,22 @@
 import { View, Text } from "react-native";
 import { styles } from "./styles";
 import { colors } from "@/styles/colors";
+import { Icon, IconProps } from "@tabler/icons-react-native";
+import React from "react";
 
-export function Step(props) {
+type Props ={
+    title: string
+    description: string
+    icon: React.ComponentType<IconProps>
+}
+
+export function Step({title, description, icon: Icon}: Props) {
   return (
     <View style={styles.container}>
       <View style={styles.details}>
-        <Text style={styles.title}>{props.title}</Text>
-        <Text style={styles.description}>{props.description}</Text>
+        <Icon size={32} color={colors.red.base}></Icon>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.description}>{description}</Text>
       </View>
     </View>
   );
--- src/components/steps/index.tsx
+++ src/components/steps/index.tsx
@@ -1,6 +1,8 @@
 import { styles } from "./styles";
 import { Text, View } from "react-native";
 import { Step } from "../step";
+import { IconMapPin, IconQrcode, IconTicket } from "@tabler/icons-react-native";
+
 
 export function Steps() {
   return (
@@ -9,10 +11,17 @@ export function Steps() {
       <Step
         title="Encontre estabelecimentos"
         description="Veja os locais mais perto de você que são parceiros da Nearby"
+        icon={IconMapPin}
       ></Step>
       <Step
         title="Ative o cupom com QR Code"
         description="Escaneie o código no estabelecimento para ganhar seus benefícios"
+        icon={IconQrcode}
+      ></Step>
+      <Step
+        title="Garanta as vantagens perto de você"
+        description="Ative os cupons onde estiver, em diferentes tipos de estabelecimentos"
+        icon={IconTicket}
       ></Step>
     </View>
   );
